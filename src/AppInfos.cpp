858c7b1015ce3b47b1262d4a4491c83d8fbc0120
nsz32 <nico@moi.com>
2020-04-24 02:06:47 +0200

    get appinfo (with icon) of newly installed applications

--- src/AppInfos.cpp
+++ src/AppInfos.cpp
@@ -5,192 +5,230 @@
 
 namespace AppInfos
 {
-	namespace
-	{
-		std::list<std::string> mXdgDataDirs;
-		Store::Map<const std::string, AppInfo*> mAppInfoIds;
-		Store::Map<const std::string, AppInfo*> mAppInfoNames;
 
-		std::string parseId(const std::string id)
+std::list<std::string> mXdgDataDirs;
+Store::Map<const std::string, AppInfo *> mAppInfoIds;
+Store::Map<const std::string, AppInfo *> mAppInfoNames;
+
+pthread_mutex_t AppInfosLock;
+
+std::string parseId(const std::string id)
+{
+	return id.substr(0, id.size() - 8);
+}
+
+void findXDGDirectories()
+{
+	char *var = getenv("XDG_DATA_DIRS");
+	if (var != NULL && var[0] != '\0')
+	{
+		Help::String::split(var, mXdgDataDirs, ':');
+	}
+
+	mXdgDataDirs.push_back("/usr/local/share");
+	mXdgDataDirs.push_back("/usr/share");
+
+	for (std::string &dirs : mXdgDataDirs)
+	{
+		dirs += "/applications/";
+	}
+
+	mXdgDataDirs.sort();
+	mXdgDataDirs.unique();
+}
+
+void loadDesktopEntry(const std::string &xdgDir, std::string filename)
+{
+	if (filename.size() < 9 || filename.substr(filename.size() - 8, std::string::npos) != ".desktop")
+		return;
+
+	std::string id = filename.substr(0, filename.size() - 8);
+	std::string path = xdgDir + id + ".desktop";
+
+	GDesktopAppInfo *gAppInfo = g_desktop_app_info_new_from_filename(path.c_str());
+
+	if (gAppInfo == NULL)
+		return;
+
+	pthread_mutex_lock(&AppInfosLock);
+
+	std::string icon;
+	char *icon_ = g_desktop_app_info_get_string(gAppInfo, "Icon");
+	if (icon_ != NULL)
+		icon = Help::String::trim(icon_);
+
+	std::string name;
+	char *name_ = g_desktop_app_info_get_string(gAppInfo, "Name");
+	if (name_ != NULL)
+		name = name_;
+
+	AppInfo *info = new AppInfo({path, icon, name});
+
+	id = Help::String::toLowercase(id);
+	mAppInfoIds.set(id, info);
+	mAppInfoIds.set(path, info); //for saved pinned groups
+
+	if (!name.empty())
+	{
+		name = Help::String::toLowercase(Help::String::trim(name));
+		if (name.find(' ') == std::string::npos)
 		{
-			return id.substr(0, id.size() - 8);
+			if (name != id)
+				mAppInfoNames.set(name, info);
 		}
 	}
 
-	void findXDGDirectories()
+	std::string exec;
+	char *exec_ = g_desktop_app_info_get_string(gAppInfo, "Exec");
+	if (exec_ != NULL && exec_[0] != '\0')
 	{
-		//1. User's .local
-		char* var = getenv("XDG_DATA_HOME");
-		std::string xdgDataHome;
-		if(var != NULL && var[0] != '\0')
-			mXdgDataDirs.push_back(var);
-		else
-			mXdgDataDirs.push_back(std::string(getenv("HOME")) + "/.local/share");
+		std::string execLine = Help::String::toLowercase(Help::String::pathBasename(Help::String::trim(exec_)));
 
-		//2. System wide
-		var = getenv("XDG_DATA_DIRS");
-		if(var != NULL && var[0] != '\0')
+		exec = Help::String::getWord(execLine, 0);
+		if (exec != "env" && exec != "exo-open")
 		{
-			Help::String::split(var, mXdgDataDirs, ':');
-		}
-		else
-		{
-			mXdgDataDirs.push_back("/usr/local/share");
-			mXdgDataDirs.push_back("/usr/share");
-		}
-
-		for(std::string& dirs : mXdgDataDirs)
-		{
-			dirs += "/applications/";
+			if (exec != id && exec != name)
+				mAppInfoNames.set(Help::String::toLowercase(exec), info);
 		}
 	}
 
-	void loadDesktopEntry(const std::string& xdgDir, std::string id)
+	std::string wmclass;
+	char *wmclass_ = g_desktop_app_info_get_string(gAppInfo, "StartupWMClass");
+	if (wmclass_ != NULL && wmclass_[0] != '\0')
 	{
-		std::string path = xdgDir + id + ".desktop";
+		wmclass = Help::String::toLowercase(Help::String::trim(wmclass_));
 
-		GDesktopAppInfo* gAppInfo = g_desktop_app_info_new_from_filename(path.c_str());
-
-		if(gAppInfo == NULL) return;
-
-		std::string icon;
-		char* icon_ = g_desktop_app_info_get_string(gAppInfo, "Icon");
-		if(icon_ != NULL)
-			icon = Help::String::trim(icon_);
-
-		std::string name;
-		char* name_ = g_desktop_app_info_get_string(gAppInfo, "Name");
-		if(name_ != NULL)
-			name = name_;
-
-		AppInfo* info = new AppInfo({path, icon, name});
-
-		id = Help::String::toLowercase(id);
-		mAppInfoIds.set(id, info);
-		mAppInfoIds.set(path, info);
-
-		if(!name.empty()) 
-		{
-			name = Help::String::toLowercase(Help::String::trim(name));
-			if(name.find(' ') == std::string::npos)
-			{
-				if(name != id)
-					mAppInfoNames.set(name, info);
-			}
-		}
-		
-		std::string exec;
-		char* exec_ = g_desktop_app_info_get_string(gAppInfo, "Exec");
-		if(exec_ != NULL && exec_[0] != '\0') 
-		{
-			std::string execLine = Help::String::toLowercase(Help::String::pathBasename(Help::String::trim(exec_)));
-
-			exec = Help::String::getWord(execLine, 0);
-			if(exec != "env" && exec != "exo-open")
-			{
-				if(exec != id && exec != name)
-					mAppInfoNames.set(Help::String::toLowercase(exec), info);
-			}
-		}
-
-		std::string wmclass;
-		char* wmclass_ = g_desktop_app_info_get_string(gAppInfo, "StartupWMClass");
-		if(wmclass_ != NULL && wmclass_[0] != '\0')
-		{
-			wmclass = Help::String::toLowercase(Help::String::trim(wmclass_));
-
-			if(wmclass != id && wmclass != name && wmclass != exec)
-				mAppInfoNames.set(wmclass, info);
-		}
-
-		/*std::cout << "ID:" << id << std::endl;
-		std::cout << "NAME:" << name << std::endl;
-		std::cout << "EXEC:" << exec << std::endl;
-		std::cout << "WMCLASS:" << wmclass << std::endl;
-		std::cout << ":" << std::endl;*/
+		if (wmclass != id && wmclass != name && wmclass != exec)
+			mAppInfoNames.set(wmclass, info);
 	}
 
-	void loadXDGDirectories()
+	pthread_mutex_unlock(&AppInfosLock);
+}
+
+void *watchXDGDirectory(void *dirPath)
+{
+	std::cout << "watch XDG dir: " << *(std::string *)dirPath << std::endl;
+
+	int fd = inotify_init();
+	int wd = inotify_add_watch(fd, ((std::string *)dirPath)->c_str(), IN_CLOSE_WRITE | IN_MOVED_TO);
+
+	int i = 0, len = 0;
+	char buf[1024];
+	struct inotify_event *event;
+
+	while (true)
 	{
-		for(std::string xdgDir : mXdgDataDirs)
+		i = 0;
+		len = read(fd, buf, 1024);
+
+		while (i < len)
 		{
-			DIR* directory = opendir(xdgDir.c_str());
-			if(directory == NULL) continue;
-			
-			struct dirent* entry;
-			std::list<std::string> matches;
-			while((entry = readdir(directory)) != NULL )
-			{
-				std::string filename = entry->d_name;
+			event = (struct inotify_event *)&buf[i];
+			std::cout << "new file detected:" << event->name << std::endl;
 
-				if(filename.size() < 9 || filename.substr(filename.size() - 8, std::string::npos) != ".desktop")
-					continue;
+			loadDesktopEntry(*(std::string *)dirPath, event->name);
 
-				std::string id = filename.substr(0, filename.size() - 8);
-
-				loadDesktopEntry(xdgDir, id);
-			}
+			i += sizeof(struct inotify_event) + event->len;
 		}
 	}
+}
 
-	void init()
+void watchXDGDirectory(std::string xdgDir)
+{
+	pthread_t thread_store;
+	std::string *arg = new std::string(xdgDir);
+
+	pthread_create(&thread_store, NULL, watchXDGDirectory, arg);
+}
+
+void loadXDGDirectories()
+{
+	for (std::string xdgDir : mXdgDataDirs)
 	{
-		findXDGDirectories();
-		loadXDGDirectories();
+		std::cout << "XDGDirectories:" << xdgDir << std::endl;
+
+		DIR *directory = opendir(xdgDir.c_str());
+		if (directory == NULL)
+			continue;
+
+		struct dirent *entry;
+		std::list<std::string> matches;
+		while ((entry = readdir(directory)) != NULL)
+		{
+			std::string filename = entry->d_name;
+
+			loadDesktopEntry(xdgDir, filename);
+		}
+
+		watchXDGDirectory(xdgDir);
+	}
+}
+
+void init()
+{
+	pthread_mutex_init(&AppInfosLock, NULL);
+
+	findXDGDirectories();
+	loadXDGDirectories();
+}
+
+AppInfo *search(std::string id)
+{
+
+	AppInfo *ai = mAppInfoIds.get(id);
+	if (ai != NULL)
+		return ai;
+
+	uint pos = id.find(' ');
+	if (pos != std::string::npos)
+	{
+		id = id.substr(0, pos);
+
+		ai = mAppInfoIds.get(id);
+		if (ai != NULL)
+			return ai;
 	}
 
-	AppInfo* search(std::string id)
+	ai = mAppInfoNames.get(id);
+	if (ai != NULL)
+		return ai;
+
+	pos = id.find(' ');
+	if (pos != std::string::npos)
 	{
-		AppInfo* ai = mAppInfoIds.get(id);
-		if(ai != NULL) return ai;
-
-		uint pos = id.find(' ');
-		if(pos != std::string::npos)
-		{
-			id = id.substr(0, pos);
-
-			ai = mAppInfoIds.get(id);
-			if(ai != NULL) return ai;
-		}
+		id = id.substr(0, pos);
 
 		ai = mAppInfoNames.get(id);
-		if(ai != NULL) return ai;
-
-		pos = id.find(' ');
-		if(pos != std::string::npos)
-		{
-			id = id.substr(0, pos);
-
-			ai = mAppInfoNames.get(id);
-			if(ai != NULL) return ai;
-		}
-
-
-		gchar*** gioPath = g_desktop_app_info_search(id.c_str());
-		
-		if(gioPath != NULL && gioPath[0] != NULL && gioPath[0][0] != NULL && gioPath[0][0][0] != '\0')
-		{
-			std::string gioId = gioPath[0][0];
-			gioId = Help::String::toLowercase(gioId.substr(0, gioId.size()-8));
-
-			ai = mAppInfoIds.get(gioId);
-
-			for(int i = 0; gioPath[i] != NULL; ++i)
-				g_strfreev(gioPath[i]);
-			g_free(gioPath);
-
-			if(ai != NULL) return ai;
-		}
-
-		return new AppInfo({"", "", id});
+		if (ai != NULL)
+			return ai;
 	}
 
-	void launch(AppInfo* appInfo) //TODO move to AppInfo struct
+	gchar ***gioPath = g_desktop_app_info_search(id.c_str());
+
+	if (gioPath != NULL && gioPath[0] != NULL && gioPath[0][0] != NULL && gioPath[0][0][0] != '\0')
 	{
-		GDesktopAppInfo* info = g_desktop_app_info_new_from_filename(appInfo->path.c_str());
-		const gchar * const* actions = g_desktop_app_info_list_actions(info);
+		std::string gioId = gioPath[0][0];
+		gioId = Help::String::toLowercase(gioId.substr(0, gioId.size() - 8));
 
-		g_app_info_launch((GAppInfo*)info, NULL, NULL, NULL);
+		ai = mAppInfoIds.get(gioId);
+
+		for (int i = 0; gioPath[i] != NULL; ++i)
+			g_strfreev(gioPath[i]);
+		g_free(gioPath);
+
+		if (ai != NULL)
+			return ai;
 	}
-	
+
+	return new AppInfo({"", "", id});
 }
+
+void launch(AppInfo *appInfo) //TODO move to AppInfo struct
+{
+	GDesktopAppInfo *info = g_desktop_app_info_new_from_filename(appInfo->path.c_str());
+	const gchar *const *actions = g_desktop_app_info_list_actions(info);
+
+	g_app_info_launch((GAppInfo *)info, NULL, NULL, NULL);
+}
+
+} // namespace AppInfos
--- src/AppInfos.hpp
+++ src/AppInfos.hpp
@@ -4,18 +4,25 @@
 #define APPINFOS_HPP
 
 #include <iostream>
+#include <pthread.h>
+#include <sys/inotify.h>
 
 #include "Helpers.hpp"
 #include "Store.tpp"
 
-struct AppInfo { const std::string path; const std::string icon; const std::string name; };
+struct AppInfo
+{
+	const std::string path;
+	const std::string icon;
+	const std::string name;
+};
 
 namespace AppInfos
 {
-	void init();
+void init();
 
-	AppInfo* search(std::string id);
-	void launch(AppInfo* appInfo);
-}
+AppInfo *search(std::string id);
+void launch(AppInfo *appInfo);
+} // namespace AppInfos
 
 #endif
