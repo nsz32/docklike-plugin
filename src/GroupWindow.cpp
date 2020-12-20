6a5e5e16c61c9c0ea81cf0d24a8b672b24020930
nsz32 <nico@moi.com>
2020-12-20 12:02:56 +0100

    Taking care of associated status when activating a group

--- src/GroupWindow.cpp
+++ src/GroupWindow.cpp
@@ -111,7 +111,8 @@ void GroupWindow::onActivate()
 	Help::Gtk::cssClassAdd(GTK_WIDGET(mGroupMenuItem->mItem), "active");
 	gtk_widget_queue_draw(GTK_WIDGET(mGroupMenuItem->mItem));
 
-	mGroup->onWindowActivate(this);
+	if(mGroupAssociated)
+		mGroup->onWindowActivate(this);
 }
 
 void GroupWindow::onUnactivate()
@@ -119,7 +120,8 @@ void GroupWindow::onUnactivate()
 	Help::Gtk::cssClassRemove(GTK_WIDGET(mGroupMenuItem->mItem), "active");
 	gtk_widget_queue_draw(GTK_WIDGET(mGroupMenuItem->mItem));
 
-	mGroup->onWindowUnactivate();
+	if(mGroupAssociated)
+		mGroup->onWindowUnactivate();
 }
 
 bool GroupWindow::getState(WnckWindowState flagMask)
